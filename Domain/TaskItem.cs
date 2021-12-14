007aac991de2cb05f7fddb5ac98a218917585176
Jakob <jakob.sorgeloos@dotnetlab.eu>
2021-12-14 13:32:10 +0100

    Add project files.

---
+++ Domain/TaskItem.cs
@@ -0,0 +1,30 @@
+﻿
+using static Common.TaskEnums;
+
+namespace Domain
+{
+    public class TaskItem
+
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PlannedDate { get; set; } 
+
+        public string Description { get; set; }
+        public TaskTypes TaskType { get; set; }
+
+        public int TaskTypeId
+        {
+            get { return TaskTypeId; }
+            set { TaskType  =(TaskTypes)value; }
+        }
+        public TaskStates TaskState { get; set; } //ToDo: Auto implement testen =TaskState.New
+
+        public int TaskStateId
+        {
+            get { return TaskStateId; }
+            set { TaskState = (TaskStates)value; }
+        } 
+        //public List<TaskItem> ChildTasks { get; set;}
+    }
+}
---
+++ ProductivityApp/Shared/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class TaskEnums
+    {
+        public enum TaskTypes
+        {
+            Do = 0,
+            Schedule = 1,
+            Delegate = 2,
+            Eliminate = 3
+        }
+
+        public enum TaskStates
+        {
+            New = 0,
+            Planned=1,
+            InProgress =2,
+            Completed =3,
+            Canceled =4,
+            Eliminated=5
+        }
+    }
+}
